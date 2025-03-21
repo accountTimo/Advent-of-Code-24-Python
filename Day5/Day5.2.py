afcb7bca254a654ab0709385c2242239c1514683
String <s1149822@student.hsleiden.nl>
2025-03-21 23:30:57 +0100

    add solution day 5

---
+++ Day5/Day5.2.py
@@ -0,0 +1,16 @@
+﻿from functools import cmp_to_key
+
+with open('Data.csv') as f:
+    rules, pages = f.read().split('\n\n')
+
+cmp = cmp_to_key(lambda x, y: -(x + '|' + y in rules))
+middle_page_numbers_sum = 0
+
+for p in pages.split():
+    p = p.split(',')
+    s = sorted(p, key=cmp)
+
+    if p != s:
+        middle_page_numbers_sum += int(s[len(s) // 2])
+
+print(middle_page_numbers_sum)
