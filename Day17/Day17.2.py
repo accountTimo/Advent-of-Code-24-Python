0d61560637d5f81fcdb61a12cc18dc960c796279
String <s1149822@student.hsleiden.nl>
2025-04-28 22:35:11 +0200

    add solutions day 17

---
+++ Day17/Day17.2.py
@@ -0,0 +1,45 @@
+﻿with open("Data.csv") as file:
+    lines = file.readlines()
+
+program = list(map(int, lines[4].split(":")[1].strip().split(",")))
+
+def get_combo_value(operand, registers):
+    if operand <= 3: return operand
+    return registers["ABC"[operand - 4]]
+
+def find_solution(target, answer):
+    if not target:
+        return answer
+
+    for t in range(8):
+        registers = {"A": (answer << 3) | t, "B": 0, "C": 0}
+        output = None
+
+        for pointer in range(0, len(program) - 2, 2):
+            op, val = program[pointer], program[pointer + 1]
+            match op:
+                case 0:
+                    if val != 3: raise ValueError
+                case 1:
+                    registers["B"] ^= val
+                case 2:
+                    registers["B"] = get_combo_value(val, registers) % 8
+                case 4:
+                    registers["B"] ^= registers["C"]
+                case 5:
+                    output = get_combo_value(val, registers) % 8
+                case 6:
+                    registers["B"] = registers["A"] >> get_combo_value(val, registers)
+                case 7:
+                    registers["C"] = registers["A"] >> get_combo_value(val, registers)
+                case _:
+                    raise ValueError
+
+        if output == target[-1]:
+            res = find_solution(target[:-1], registers["A"])
+            if res is not None:
+                return res
+    return None
+
+solution = find_solution(program, 0)
+print(solution)
---
+++ Day17/Day17.py
@@ -0,0 +1,35 @@
+﻿with open('Data.csv') as f:
+    data = f.read().splitlines()
+
+registers = {line[9]: int(line.split(':')[1]) for line in data if line.startswith('Register')}
+program = list(map(int, next(line.split(':')[1].split(',') for line in data if line.startswith('Program'))))
+
+def combo(v):
+    return v if v <= 3 else registers['ABC'[v-4]]
+
+ip, output = 0, []
+
+while ip < len(program):
+    op, val = program[ip], program[ip+1]
+
+    match op:
+        case 0 | 6 | 7:
+            target = 'A' if op == 0 else 'B' if op == 6 else 'C'
+            registers[target] = registers['A'] // (2 ** combo(val))
+            ip += 2
+        case 1:
+            registers['B'] ^= val
+            ip += 2
+        case 2:
+            registers['B'] = combo(val) % 8
+            ip += 2
+        case 3:
+            ip = val if registers['A'] != 0 else ip + 2
+        case 4:
+            registers['B'] ^= registers['C']
+            ip += 2
+        case 5:
+            output.append(str(combo(val) % 8))
+            ip += 2
+
+print(','.join(output))
