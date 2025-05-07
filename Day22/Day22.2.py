9f7f2c4344b7072b577329d9162214ad66f07b3e
String <s1149822@student.hsleiden.nl>
2025-05-07 14:13:29 +0200

    add solutions day 22

---
+++ Day22/Day22.2.py
@@ -0,0 +1,36 @@
+﻿with open("Data.csv", "r") as file:
+    lines = file.readlines()
+
+def secretStep(secret):
+    secret = (secret * 64 ^ secret) % 16777216
+    secret = (secret // 32 ^ secret) % 16777216
+    secret = (secret * 2048 ^ secret) % 16777216
+    return secret
+
+def findMax(sequence_totals):
+    return max(sequence_totals, key=sequence_totals.get)
+
+def findMaxBananas(lines):
+    sequence_totals = {}
+    for line in lines:
+        secret_number = int(line)
+        price_list = [secret_number % 10]
+        for _ in range(2000):
+            secret_number = secretStep(secret_number)
+            price_list.append(secret_number % 10)
+        tracked_sequences = set()
+        for index in range(len(price_list) - 4):
+            p1, p2, p3, p4, p5 = price_list[index:index + 5]
+            price_change = (p2 - p1, p3 - p2, p4 - p3, p5 - p4)
+            if price_change not in tracked_sequences:
+                tracked_sequences.add(price_change)
+                if price_change not in sequence_totals:
+                    sequence_totals[price_change] = 0
+                sequence_totals[price_change] += p5
+    best_sequence = findMax(sequence_totals)
+    return best_sequence, sequence_totals[best_sequence]
+
+best_sequence, max_bananas = findMaxBananas(lines)
+
+print("Best sequence: ", best_sequence)
+print("Max bananas: ", max_bananas)
---
+++ Day22/Day22.py
@@ -0,0 +1,28 @@
+﻿def generate_secret_number(secret_number):
+    secret_number ^= (secret_number * 64)
+    secret_number %= 16777216
+
+    secret_number ^= (secret_number // 32)
+    secret_number %= 16777216
+
+    secret_number ^= (secret_number * 2048)
+    secret_number %= 16777216
+
+    return secret_number
+
+def calculate_2000th_secret_number(initial_secret_number):
+    secret_number = initial_secret_number
+    for _ in range(2000):
+        secret_number = generate_secret_number(secret_number)
+    return secret_number
+
+total = 0
+with open("Data.csv", "r") as file:
+    for line in file.read().splitlines():
+
+        initial_secret_number = int(line)
+
+        result = calculate_2000th_secret_number(initial_secret_number)
+        total += result
+
+print(total)
